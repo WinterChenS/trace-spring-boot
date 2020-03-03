7787cdc34f3e2cfdc7b00baae9f80c65a65233ff
winterchen <1085143002@qq.com>
2020-03-03 12:47:43 +0800

    init

---
+++ trace-spring-boot-starter/src/main/java/com/winterchen/tracespringbootstarter/TraceSpringBootStarterApplication.java
@@ -0,0 +1,13 @@
+package com.winterchen.tracespringbootstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TraceSpringBootStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TraceSpringBootStarterApplication.class, args);
+    }
+
+}
---
+++ trace-spring-boot-starter/src/test/java/com/winterchen/tracespringbootstarter/TraceSpringBootStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.winterchen.tracespringbootstarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TraceSpringBootStarterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

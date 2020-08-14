e8462a0f58384d4cf0af922a4a68f4e64aa58b4f
WinterChenS <1085143002@qq.com>
2020-08-14 09:07:10 +0100

    修复Hystrix并发策略冲突的问题

--- trace-spring-boot-starter/src/main/java/com/winterchen/trace/starter/config/HystrixAutoConfiguration.java
+++ trace-spring-boot-starter/src/main/java/com/winterchen/trace/starter/config/HystrixAutoConfiguration.java
@@ -1,19 +1,17 @@
 package com.winterchen.trace.starter.config;
 
-import com.netflix.hystrix.strategy.HystrixPlugins;
 import com.netflix.hystrix.strategy.concurrency.HystrixConcurrencyStrategy;
 import com.winterchen.trace.starter.instrument.hystrix.TraceHystrixConcurrencyStrategy;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import javax.annotation.PostConstruct;
 
 @Configuration
 @ConditionalOnClass(value = {HystrixConcurrencyStrategy.class})
 public class HystrixAutoConfiguration {
 
-    @PostConstruct
-    public void init() {
-        HystrixPlugins.getInstance().registerConcurrencyStrategy(new TraceHystrixConcurrencyStrategy());
+    @Bean
+    public TraceHystrixConcurrencyStrategy traceHystrixConcurrencyStrategy() {
+        return new TraceHystrixConcurrencyStrategy();
     }
 }
--- trace-spring-boot-starter/src/main/java/com/winterchen/trace/starter/instrument/hystrix/TraceHystrixConcurrencyStrategy.java
+++ trace-spring-boot-starter/src/main/java/com/winterchen/trace/starter/instrument/hystrix/TraceHystrixConcurrencyStrategy.java
@@ -1,20 +1,77 @@
 package com.winterchen.trace.starter.instrument.hystrix;
 
+import com.netflix.hystrix.HystrixThreadPoolKey;
+import com.netflix.hystrix.HystrixThreadPoolProperties;
+import com.netflix.hystrix.strategy.HystrixPlugins;
 import com.netflix.hystrix.strategy.concurrency.HystrixConcurrencyStrategy;
+import com.netflix.hystrix.strategy.concurrency.HystrixRequestVariable;
+import com.netflix.hystrix.strategy.concurrency.HystrixRequestVariableLifecycle;
+import com.netflix.hystrix.strategy.eventnotifier.HystrixEventNotifier;
+import com.netflix.hystrix.strategy.executionhook.HystrixCommandExecutionHook;
+import com.netflix.hystrix.strategy.metrics.HystrixMetricsPublisher;
+import com.netflix.hystrix.strategy.properties.HystrixPropertiesStrategy;
+import com.netflix.hystrix.strategy.properties.HystrixProperty;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.slf4j.MDC;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.Callable;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
 
 /**
  * Hystrix线程池隔离支持日志链路跟踪
+ * 
  * @author winterchen 2020/2/28
  */
 public class TraceHystrixConcurrencyStrategy extends HystrixConcurrencyStrategy {
 
+    private static final Logger log = LoggerFactory.getLogger(TraceHystrixConcurrencyStrategy.class);
+
+    private HystrixConcurrencyStrategy delegate;
+
+    public TraceHystrixConcurrencyStrategy() {
+
+        try {
+            this.delegate = HystrixPlugins.getInstance().getConcurrencyStrategy();
+            if (this.delegate instanceof TraceHystrixConcurrencyStrategy) {
+                // Welcome to singleton hell...
+                return;
+            }
+            final HystrixCommandExecutionHook commandExecutionHook = HystrixPlugins.getInstance().getCommandExecutionHook();
+            final HystrixEventNotifier eventNotifier = HystrixPlugins.getInstance().getEventNotifier();
+            final HystrixMetricsPublisher metricsPublisher = HystrixPlugins.getInstance().getMetricsPublisher();
+            final HystrixPropertiesStrategy propertiesStrategy = HystrixPlugins.getInstance().getPropertiesStrategy();
+            this.logCurrentStateOfHystrixPlugins(eventNotifier, metricsPublisher, propertiesStrategy);
+            HystrixPlugins.reset();
+            HystrixPlugins.getInstance().registerConcurrencyStrategy(this);
+            HystrixPlugins.getInstance().registerCommandExecutionHook(commandExecutionHook);
+            HystrixPlugins.getInstance().registerEventNotifier(eventNotifier);
+            HystrixPlugins.getInstance().registerMetricsPublisher(metricsPublisher);
+            HystrixPlugins.getInstance().registerPropertiesStrategy(propertiesStrategy);
+        } catch (final Exception e) {
+            log.error("Failed to register Sleuth Hystrix Concurrency Strategy", e);
+        }
+
+    }
+
+    private void logCurrentStateOfHystrixPlugins(final HystrixEventNotifier eventNotifier,
+            final HystrixMetricsPublisher metricsPublisher, final HystrixPropertiesStrategy propertiesStrategy) {
+        if (log.isDebugEnabled()) {
+            log.debug("Current Hystrix plugins configuration is [" + "concurrencyStrategy [" + this.delegate + "],"
+                    + "eventNotifier [" + eventNotifier + "]," + "metricPublisher [" + metricsPublisher + "],"
+                    + "propertiesStrategy [" + propertiesStrategy + "]," + "]");
+            log.debug("Registering Sleuth Hystrix Concurrency Strategy.");
+        }
+    }
+
     @Override
-    public <T> Callable<T> wrapCallable(Callable<T> callable) {
+    public <T> Callable<T> wrapCallable(final Callable<T> callable) {
         return new MdcAwareCallable(callable, MDC.getCopyOfContextMap());
     }
 
@@ -24,7 +81,7 @@ public class TraceHystrixConcurrencyStrategy extends HystrixConcurrencyStrategy 
 
         private final Map<String, String> contextMap;
 
-        public MdcAwareCallable(Callable<T> callable, Map<String, String> contextMap) {
+        public MdcAwareCallable(final Callable<T> callable, final Map<String, String> contextMap) {
             this.delegate = callable;
             this.contextMap = contextMap != null ? contextMap : new HashMap();
         }
@@ -39,4 +96,32 @@ public class TraceHystrixConcurrencyStrategy extends HystrixConcurrencyStrategy 
             }
         }
     }
+
+    @Override
+    public ThreadPoolExecutor getThreadPool(
+            final HystrixThreadPoolKey threadPoolKey,
+			final HystrixProperty<Integer> corePoolSize,
+			final HystrixProperty<Integer> maximumPoolSize,
+			final HystrixProperty<Integer> keepAliveTime, final TimeUnit unit,
+			final BlockingQueue<Runnable> workQueue) {
+		return this.delegate.getThreadPool(threadPoolKey, corePoolSize, maximumPoolSize,
+				keepAliveTime, unit, workQueue);
+	}
+
+	@Override
+	public ThreadPoolExecutor getThreadPool(final HystrixThreadPoolKey threadPoolKey,
+			final HystrixThreadPoolProperties threadPoolProperties) {
+		return this.delegate.getThreadPool(threadPoolKey, threadPoolProperties);
+	}
+
+	@Override
+	public BlockingQueue<Runnable> getBlockingQueue(final int maxQueueSize) {
+		return this.delegate.getBlockingQueue(maxQueueSize);
+	}
+
+	@Override
+	public <T> HystrixRequestVariable<T> getRequestVariable(
+			final HystrixRequestVariableLifecycle<T> rv) {
+		return this.delegate.getRequestVariable(rv);
+	}
 }
